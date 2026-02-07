0253ba90ee7621358b592cdd15e1c0e63b340dc2
Ankushkumardas <anksuh114@gmail.com>
2026-02-08 04:00:07 +0530

    almost all teh authroutes are been completed will do more chnages as required

--- backend/src/models/NotificationSchema.js
+++ backend/src/models/NotificationSchema.js
@@ -13,7 +13,7 @@ const NotificationSchema = new mongoose.Schema({
 
     type: {
         type: String,
-        enum: ["SYSTEM", "MESSAGE", "BILLING"],
+        enum: ["SYSTEM", "MESSAGE", "BILLING","USER"],
     },
 
     message: String,
