d6fd5ecbac0bb694139cb76d2ef68c6b353b2286
Ankushkumardas <anksuh114@gmail.com>
2026-02-08 03:51:04 +0530

    almost all teh authroutes are been completed will do more chnages as required

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,21 +1,22 @@
 import express from 'express';
-import { authenticate } from '../middleware/authMiddleware.js';
+import { authenticate, authorize } from '../middleware/authMiddleware.js';
 import { checkTenant } from '../middleware/tenantMiddleware.js';
-import { registerOwner, resendVerificationEmail, sendInvite, verifyOwnerEmail, login, refreshToken, logout, getProfile, forgotPassword, resetPassword, changePasword, acceptInvite } from '../controller/authController.js';
+import { registerOwner, resendVerificationEmail, sendInvite, verifyOwnerEmail, login, refreshToken, logout, getProfile, forgotPassword, resetPassword, changePasword, acceptInvite, updateUserRole, updateProfileData } from '../controller/authController.js';
 
 const router = express.Router();
 
 router.post("/register-owner", registerOwner)
 router.post("/verify-owner-email", verifyOwnerEmail)
 router.post("/resend-verification-email", resendVerificationEmail)
-router.post("/send-invite", authenticate, checkTenant, sendInvite)
+router.post("/send-invite", authenticate, checkTenant, authorize("OWNER"), sendInvite)
 router.post("/accept-invite", authenticate, checkTenant, acceptInvite)
 router.get("/profile", authenticate, checkTenant, getProfile)
 router.post("/login", login)
 router.post("/refresh", refreshToken)
 router.post("/logout", logout)
 router.post("/forgot-password", forgotPassword)
-router.post("/reset-password", resetPassword)
+router.post("/reset-password", authenticate, checkTenant, resetPassword)
 router.post("/change-password", authenticate, checkTenant, changePasword)
-
+router.put("/update-role", authenticate, checkTenant, authorize("OWNER"), updateUserRole)
+router.put("/update-profile", authenticate, checkTenant, updateProfileData)
 export default router;
