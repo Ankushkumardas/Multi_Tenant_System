6834d338f77638928e003c633e00680dd79527d2
Ankushkumardas <anksuh114@gmail.com>
2026-03-05 00:14:17 +0530

    redis

--- backend/src/utils/redis.js
+++ backend/src/utils/redis.js
@@ -1,6 +1,9 @@
 import { createClient } from "redis";
 
-const client = createClient();
+// const client = createClient();
+const client = createClient({
+  url: process.env.REDIS_URL || "redis://localhost:6379",
+});
 
 client.on("error", (err) => {
   console.error("Redis Error:", err);
